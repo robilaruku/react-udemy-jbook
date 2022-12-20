080349ef2bc7a602fb175cfe04ebd4ddda35ba77
robi <robidream20@gmail.com>
2022-12-20 13:44:22 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -29,10 +29,12 @@ const App = () => {
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
     });
 
-    // console.log(result);
-    
     setCode(result.outputFiles[0].text);
   };
 
