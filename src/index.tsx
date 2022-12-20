7e5b4c005b766cad936d9907971b0c002a228ada
robi <robidream20@gmail.com>
2022-12-20 11:05:31 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import *  as esbuild from 'esbuild-wasm';
+import * as esbuild from "esbuild-wasm";
 import { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
-  
+
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: './esbuild.wasm'
+      wasmURL: "./esbuild.wasm",
     });
   };
 
@@ -23,12 +24,16 @@ const App = () => {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015'
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
-      
-    setCode(result.code);
+
+    // console.log(result);
+    
+    setCode(result.outputFiles[0].text);
   };
 
   return (
