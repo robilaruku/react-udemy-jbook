206a2c3e383e0c2580605910445375c8b07e61dd
Robi Irhamni <robidream20@gmail.com>
2022-12-25 19:01:17 +0700

    add monaco template

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -64,13 +66,17 @@ const App = () => {
 
   return (
     <>
+      <CodeEditor />
       <textarea
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => 
+          setInput(e.target.value)
+        }
       ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
+      <pre>{code}</pre>
       <br></br>
       <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
     </>
