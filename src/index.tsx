83e7db461fa45e57de8b18a5e02f3ed9655899b4
Robi Irhamni <robidream20@gmail.com>
2022-12-31 10:08:30 +0700

    update react resizable

--- src/index.tsx
+++ src/index.tsx
@@ -1,62 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const ref = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
-  };
-
   return (
     <>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <br></br>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </>
   );
 };
