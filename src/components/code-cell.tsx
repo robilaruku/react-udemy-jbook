7de4a175fdb00aeef5228156844f578efe8e43f6
Robi Irhamni <robidream20@gmail.com>
2023-01-17 16:14:49 +0700

    update error handler

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,12 +6,14 @@ import Resizable from "./resizable";
 
 const CodeCell = () => {
   const [input, setInput] = useState("");
+  const [err, setErr] = useState("");
   const [code, setCode] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 750);
 
     return () => {
@@ -29,7 +31,7 @@ const CodeCell = () => {
               onChange={(value) => setInput(value)}
             />
           </Resizable>
-          <Preview code={code} />
+          <Preview code={code} err={err} />
         </div>
       </Resizable>
     </>
