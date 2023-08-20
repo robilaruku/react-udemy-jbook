56ba194c4d399b0f12b5307b68c1aa08af15d12d
Robi Irhamni <robidream20@gmail.com>
2023-08-20 19:25:02 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
 import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 const App = () => {
   return (
     <>
-      <TextEditor />
+      <Provider store={store}>
+        <div>
+          <TextEditor />
+        </div>
+      </Provider>
     </>
   );
 };
