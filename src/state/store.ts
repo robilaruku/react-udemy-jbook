4afe50055f4a920ca1185ad60a7f6bd23959f13d
Robi Irhamni <robidream20@gmail.com>
2023-10-18 18:38:40 +0700

    update cells reducers

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,4 +2,4 @@ import { applyMiddleware, createStore } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(reducers, applyMiddleware(thunk));
